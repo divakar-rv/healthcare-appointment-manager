7f010ee092ce9e02a6ba52de92ae4125497acac5
Divakar V <divakarrv15@gmail.com>
2026-08-23 00:50:51 +0530

    Add authentication middleware and protected test route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,5 +4,9 @@ const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+const { authenticate, authorize } = require('../middleware/auth');
 
+router.get('/me', authenticate, (req, res) => {
+  res.json({ message: 'You are authenticated', user: req.user });
+});
 module.exports = router;
